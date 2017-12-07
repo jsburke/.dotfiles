27bfba0e036aca6fd21d44779b0f712d248f0312
jsburke <jsburke@bu.edu>
2017-12-06 20:42:02 -0500

    l=ll

--- bash_down.sh
+++ bash_down.sh
@@ -5,9 +5,9 @@ rm -f ~/.bash_alias
 rm -f ~/.bash_fn
 # not deleting bash_local since it will vary by machine
 
-cp $BASH_SETUP/bashrc       ~/.bashrc
-cp $BASH_SETUP/bash_alias   ~/.bash_alias
-cp $BASH_SETUP/bash_fn      ~/.bash_fn
+cp bashrc       ~/.bashrc
+cp bash_alias   ~/.bash_alias
+cp bash_fn      ~/.bash_fn
 
 # open Pandora's box
 source ~/.bashrc
