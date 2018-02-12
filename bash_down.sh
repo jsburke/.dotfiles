83dc56b1594d168dd59c6615ca6fdc48960a626b
jsburke <jsburke@bu.edu>
2018-02-11 21:13:04 -0500

    update scripts for ctags and vim

--- bash_down.sh
+++ bash_down.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-rm -f ~/.bashrc
-rm -f ~/.bash_alias
-rm -f ~/.bash_fn
+rm -f  ~/.bashrc
+rm -f  ~/.bash_alias
+rm -f  ~/.bash_fn
+
+rm -f  ~/.ctags
+rm -f  ~/.vimrc
+rm -rf ~/.vim
 # not deleting bash_local since it will vary by machine
 
 cp bashrc       ~/.bashrc
 cp bash_alias   ~/.bash_alias
 cp bash_fn      ~/.bash_fn
+
+cp ctags        ~/.ctags
+cp vimrc        ~/.vimrc
+cp vim          ~/.vim
 
 # open Pandora's box
 source ~/.bashrc
--- bash_up.sh
+++ bash_up.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-rm -f $BASH_SETUP/bashrc
-rm -f $BASH_SETUP/bash_alias
-rm -f $BASH_SETUP/bash_fn
+rm -f  $BASH_SETUP/bashrc
+rm -f  $BASH_SETUP/bash_alias
+rm -f  $BASH_SETUP/bash_fn
+
+rm -f  $BASH_SETUP/ctags
+rm -f  $BASH_SETUP/vimrc
+rm -rf $BASH_SETUP/vim
 # not deleting bash_local since it will vary by machine
 
 cp ~/.bashrc       $BASH_SETUP/bashrc
 cp ~/.bash_alias   $BASH_SETUP/bash_alias
 cp ~/.bash_fn      $BASH_SETUP/bash_fn
+
+cp ~/.ctags        $BASH_SETUP/ctags
+cp ~/.vimrc        $BASH_SETUP/vimrc
+cp ~/.vim          $BASH_SETUP/vim
